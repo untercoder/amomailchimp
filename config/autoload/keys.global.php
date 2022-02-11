97f69126bf29a71e1af345951860a4a1e52b8ad5
Alex Mokrynskiy <nosekpt@gmail.com>
2022-02-11 17:51:42 +0300

    Ветка для тикета SHOOL-213 и Мердж

---
+++ config/autoload/keys.global.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'keys' => [
+        "secretKey" => "cHsgp8kOV4Viqozb5W6VxXlMH8QXSq0oWOyLU7fYw0WF7Yr6qjlyitsEuu8gNm2R",
+        "integrationId" => "1a4d6b39-0afd-4989-9c10-73d500c9a9a5",
+        "redirectURI" => "https://f8c3-80-250-213-62.ngrok.io/api/main",
+    ],
+
+];
---
+++ src/App/ConfigProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Factory\MainHandlerFactory;
+use App\Factory\MyTestFactory;
+use App\Handler\MainHandler;
+use App\Handler\MyTestHandler;
+use Whoops\Handler\Handler;
+
+/**
+ * The configuration provider for the App module
+ *
+ * @see https://docs.laminas.dev/laminas-component-installer/
+ */
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates'    => $this->getTemplates(),
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     */
+    public function getDependencies(): array
+    {
+        return [
+            'invokables' => [
+            ],
+            'factories' => [
+                MyTestHandler::class => MyTestFactory::class,
+                MainHandler::class => MainHandlerFactory::class,
+            ],
+        ];
+    }
+    /**
+     * Returns the templates configuration
+     */
+    public function getTemplates(): array
+    {
+        return [
+            'paths' => [
+                'app'    => ['templates/app'],
+                'error'  => ['templates/error'],
+                'layout' => ['templates/layout'],
+            ],
+        ];
+    }
+
+}
