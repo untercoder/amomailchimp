df69ca1fe26c17ee7ba431b02e1fb371cb091ac2
Alex Mokrynskiy <nosekpt@gmail.com>
2022-02-25 16:06:54 +0300

    MailchimpIntAmo v0.1(bugfix)

--- src/App/Traits/UpdateContactStausTrait.php
+++ src/App/Traits/UpdateContactStausTrait.php
@@ -17,35 +17,49 @@ trait UpdateContactStausTrait
                'name' => $data[0]['name'],
                'email' => $email['value'],
            ]);
-       }
 
-        foreach ($data[0]['custom_fields'][0]['values'] as $email) {
-            $this->clientMailchimp->lists->setListMember(self::MAILCHIMP_ID, md5($email['value']), [
-                'email_address' => $email['value'],
-                'status_if_new' => 'subscribed',
-                'merge_fields' => [
-                    'FNAME' => 'Contact '.$data[0]['name'],
-                ],
-            ]);
-        }
+           $this->updateContactFromMailchimp($email['value'], $data[0]['name']);
+       }
 
     }
 
     public function updateContact(int $owner, array $data):void {
-        print_r($data['custom_fields']);
-        $this->deleteContact($data);
+        $contactId = $data[0]['id'];
+        $contacts = Contact::where('contact_id', '=', $contactId)->get();
+        foreach ($contacts as $contact){
+            $contact->delete();
+        }
         $this->addContact($owner, $data);
     }
 
     public function deleteContact(array $data):void {
         $contactId = $data[0]['id'];
-        $contact = Contact::where('contact_id', '=', $contactId)->first();
+        $contacts = Contact::where('contact_id', '=', $contactId)->get();
+        foreach ($contacts as $contact){
+            $email = $contact->email;
+            $contact->delete();
+            $this->deleteContactFromMailchimp($email);
+        }
+    }
+
+    public function deleteContactFromMailchimp(string $email){
+        echo $email;
         try {
-            $this->clientMailchimp->lists->deleteListMemberPermanent(self::MAILCHIMP_ID, md5($contact->email));
+            $this->clientMailchimp->lists->deleteListMemberPermanent(self::MAILCHIMP_ID, md5($email));
         } catch(ClientException $e) {
             return;
         }
-        $contact->delete();
+    }
+
+    public function updateContactFromMailchimp($email, $name){
+        echo $email;
+        $this->clientMailchimp->lists->setListMember(self::MAILCHIMP_ID, md5($email), [
+            'email_address' => $email,
+            'status_if_new' => 'subscribed',
+            'merge_fields' => [
+                'FNAME' => $name,
+            ],
+        ]);
     }
 
 }
